c1db00d16d9a81b3017ba5a715f5a5c2e84830c5
David Konsumer <konsumer@jetboystudio.com>
2026-01-12 15:49:26 -0800

    update nvm

--- install.sh
+++ install.sh
@@ -74,6 +74,8 @@ curl -Ss https://sh.rustup.rs | sh
 
 echo "Installing nvm (nodejs)"
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 nvm install node
 nvm use node
 
